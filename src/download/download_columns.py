cea8cda461c265c7f988afbc495d7718653947de
Kelly Christensen <kelly.christensen@sciencespo.fr>
2023-11-18 14:23:48 +0100

    improve ux

--- src/download/download_columns.py
+++ src/download/download_columns.py
@@ -1,7 +1,8 @@
 from typing import List
 
 from rich import print
-from rich.console import Console
+from rich.console import Console, Group
+from rich.panel import Panel
 from rich.prompt import Confirm, Prompt
 from rich.table import Table as RichTable
 from sqlalchemy import Engine, select
@@ -15,41 +16,65 @@ class DownloadColumns(Download):
     ) -> None:
         # Initialize the base Download class
         super().__init__(engine, table_name, download_directory)
+        self.table_columns = [str(c.name) for c in self.table.columns]
+        self.outfile = self.outdir.joinpath("selection_" + self.table.name + ".csv")
 
         # Set up the columns' select statement
-        selected_columns = self.make_selection()
-        statement = select(*selected_columns)
+        statement = self.make_selection()
+        print(statement)
 
         # Execute the selection and write to outfile
         self.select(statement=statement)
 
     def make_selection(self):
-        columns = {c.name: c for c in self.table.columns}
-        selected_columns = {}
-        still_selecting = True
-        while still_selecting:
-            Console().clear()
-            print("Currently selected: ", set(selected_columns.keys()))
-            prompt_choices = columns.copy()
-            {prompt_choices.pop(name) for name in selected_columns.keys()}
-            self.render_table(selectable_columns=list(prompt_choices.keys()))
-            selected_column_name = Prompt.ask(
-                "Column", choices=list(prompt_choices.keys()), show_choices=False
-            )
-            selected_columns.update(
-                {selected_column_name: columns[selected_column_name]}
-            )
-            print("Selected: ", set(selected_columns.keys()))
-            still_selecting = Confirm.ask("Do you want to select more columns?")
-        columns = list(selected_columns.values())
-        selection = []
-        for column in columns:
-            selection.append(getattr(self.table.c, column.name))
-        return selection
+        self.console = Console()
 
-    def render_table(self, selectable_columns: List[str]):
-        rich_table = RichTable(title="Column options")
-        rich_table.add_column(header="names")
-        [rich_table.add_row(name) for name in selectable_columns]
-        console = Console()
-        console.print(rich_table)
+        selected_columns = []
+        remaining_columns = self.display_choices(selected_columns=selected_columns)
+        selection = Prompt.ask(
+            "Column to select", choices=remaining_columns, show_choices=False
+        )
+        selected_columns.append(selection)
+
+        still_selecting = Confirm.ask("Do you want to select more columns?")
+
+        while still_selecting:
+            remaining_columns = self.display_choices(selected_columns=selected_columns)
+            selection = Prompt.ask(
+                "Column to select", choices=remaining_columns, show_choices=False
+            )
+            selected_columns.append(selection)
+            still_selecting = Confirm.ask("Do you want to select more columns?")
+
+        self.display_choices(selected_columns=selected_columns)
+
+        return self.make_statement(selected_columns)
+
+    def display_choices(self, selected_columns: List[str]) -> List[str]:
+        self.console.clear()
+
+        remaining_columns = self.determine_remaining(selected_columns)
+        remaining_table = RichTable(title="Remaining")
+        remaining_table.add_column("Columns")
+        [remaining_table.add_row(c) for c in remaining_columns]
+
+        selected_table = RichTable(title="Selected")
+        selected_table.add_column("Columns")
+        [selected_table.add_row(c) for c in selected_columns]
+
+        panel_group = Group(
+            remaining_table,
+            selected_table,
+        )
+        self.console.print(Panel(panel_group))
+
+        return remaining_columns
+
+    def determine_remaining(self, selected_columns: List[str]) -> List[str]:
+        return [c for c in self.table_columns if c not in selected_columns]
+
+    def make_statement(self, selected_columns: List[str]):
+        columns = []
+        for column_name in selected_columns:
+            columns.append(getattr(self.table.c, column_name))
+        return select(*columns)
